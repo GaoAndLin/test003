fa5e1e8b4ce592a19af0e53992a85f902f0f3d38
GaoAndLin <1521337284@qq.com>
2019-05-08 16:39:21 +0800

    测试新的提交

--- src/travel/Try.java
+++ src/travel/Try.java
@@ -1,1 +1,44 @@
 package travel;
+
+public class Try {
+
+	public static void main(String[] args) {
+		/*
+		 * Ioc:默认id为类名小写开头
+		 * 
+		 * @Component:普通类
+		 * 
+		 * @repository:Dao类
+		 * 
+		 * @Service:Service
+		 * 
+		 * @Controller:Controller
+		 * 
+		 * DI
+		 * 
+		 * @Autowired:按类型找
+		 * 
+		 * @Qualifier:按id找entityDao
+		 * 
+		 * @Resource:按id找
+		 */
+
+		/*
+		 * AOP
+		 * 
+		 * @Pointcut:切点
+		 * 
+		 * @Aspect:切面
+		 * 
+		 * @Before:前置增强
+		 * 
+		 * @AfterReturning:后置增强
+		 * 
+		 * @AfterThrowing:异常增强
+		 * 
+		 * @After:最终增强
+		 * 
+		 * @Around:环绕增强
+		 */
+	}
+}
